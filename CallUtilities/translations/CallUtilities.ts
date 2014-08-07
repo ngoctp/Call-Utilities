e56ed0479f3fffaabb2c4969c8d420f009d79a69
Tran Phuong Ngoc <phuongngoc@gmail.com>
2014-08-08 01:07:31 +0700

    Remove translation

--- CallUtilities/translations/CallUtilities.ts
+++ CallUtilities/translations/CallUtilities.ts
@@ -1,17 +1,4 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0">
-<context>
-    <name>main</name>
-    <message>
-        <location filename="../assets/main.qml" line="23"/>
-        <source>CallUtilities Service says Hello!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../assets/main.qml" line="26"/>
-        <source>Resend Notification</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
 </TS>
